b9a456187ba525e4c9f26853dc157965a833032d
hanhaohh <sxhanhao@gmail.com>
2015-11-22 12:24:11 -0500

    new changes

---
+++ main/url.py
@@ -0,0 +1,22 @@
+"""
+Definition of urls for DjangoWebProject.
+"""
+
+from datetime import datetime
+from django.conf.urls import patterns, url
+from app.forms import BootstrapAuthenticationForm
+# Uncomment the next lines to enable the admin:
+# from django.conf.urls import include
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 'main.views.jobIndex', name='jobhome'),
+
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
