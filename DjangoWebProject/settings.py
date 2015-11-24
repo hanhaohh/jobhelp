acf33374b461890e777f62b2f9cc81e46423c878
hanhaohh <sxhanhao@gmail.com>
2015-11-23 23:42:07 -0500

    database

--- DjangoWebProject/settings.py
+++ DjangoWebProject/settings.py
@@ -18,29 +18,29 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-# DATABASES = {
-#         'default':{
-#         'ENGINE': 'django.db.backends.mysql',
-#         'HOST': 'us-cdbr-azure-east-a.cloudapp.net',
-#         'USER': 'b69b5911ae9066',
-#         'NAME': 'sweatjob_sql',
-#         'PASSWORD': 'a456504e',
-#         # 'OPTIONS': {'ssl': {'ca':'/Users/haohan/Desktop/cvjob/ssl/cleardb-ca.pem', 
-#         # 'cert':'/Users/haohan/Desktop/cvjob/ssl/d4f1130ec88edd-cert.pem',
-#         # 'key':'/Users/haohan/Desktop/cvjob/ssl/d4f1130ec88edd-key.pem'}},
-# }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': path.join(PROJECT_ROOT, 'db.sqlite3'),
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
+        'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'us-cdbr-azure-east-a.cloudapp.net',
+        'USER': 'b69b5911ae9066',
+        'NAME': 'sweatjob_sql',
+        'PASSWORD': 'a456504e',
+        'OPTIONS': {'ssl': {'ca':'/Users/haohan/Desktop/cvjob/ssl/cleardb-ca.pem', 
+        'cert':'/Users/haohan/Desktop/cvjob/ssl/d4f1130ec88edd-cert.pem',
+        'key':'/Users/haohan/web/nice/jobhelp/ssl/client-key-nopass.pem'}},
 }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': path.join(PROJECT_ROOT, 'db.sqlite3'),
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
 
 LOGIN_URL = '/login'
 
